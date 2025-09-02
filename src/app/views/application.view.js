493e9e5a8a6559780b3fbd158dd3ee34a56b76cb
AshleyThomasDevelopment <ashleythomasdevelopment@gmail.com>
2025-09-01 20:25:15 -0700

    Brings in assets, css, creates basic project structure

---
+++ src/app/views/application.view.js
@@ -0,0 +1,13 @@
+import FooterView from "./footer.view"
+import FrontPageView from "./front-page.view"
+import HeaderView from "./header.view"
+
+export default function ApplicationView() {
+  return (
+    <>
+      <HeaderView />
+      <FrontPageView />
+      <FooterView />
+    </>
+  )
+}
---
+++ src/app/views/front-page.view.js
@@ -0,0 +1,13 @@
+import CTA from "../components/cta.component"
+import Hero from "../components/hero.component"
+import Logos from "../components/logos.component"
+
+export default function FrontPageView() {
+  return (
+   <>
+    <CTA />
+    <Hero />
+    <Logos />
+   </>
+  );
+}
